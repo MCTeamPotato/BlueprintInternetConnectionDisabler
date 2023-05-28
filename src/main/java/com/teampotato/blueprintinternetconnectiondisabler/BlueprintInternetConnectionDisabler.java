2eace1ffbeb5545fce64d3c3192c5b65be620df8
Kasualix <670067575@qq.com>
2023-05-29 03:09:52 +0800

    Initial commit

---
+++ src/main/java/com/teampotato/blueprintinternetconnectiondisabler/BlueprintInternetConnectionDisabler.java
@@ -0,0 +1,8 @@
+package com.teampotato.blueprintinternetconnectiondisabler;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BlueprintInternetConnectionDisabler.ID)
+public class BlueprintInternetConnectionDisabler {
+    public static final String ID = "blueprintinternetconnectiondisabler";
+}
